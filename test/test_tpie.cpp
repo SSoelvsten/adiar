c99996ff72d3a518112d4b771f76e31d87d5bf36
Steffan Sølvsten <201505832@post.au.dk>
2020-06-17 13:58:34 +0200

    Categorize TPIE unit tests and add tests/examples for temporary files

--- test/test_tpie.cpp
+++ test/test_tpie.cpp
@@ -1,5 +1,5 @@
 #include <tpie/tpie.h>
-#include <tpie/tpie_log.h>
+#include <tpie/tempname.h>
 #include <tpie/sort.h>
 #include <tpie/priority_queue.h>
 
@@ -32,121 +32,199 @@ go_bandit([]() {
         auto test_stream_1_name = "test_stream_1.tpie";
         create_test_stream_1(test_stream_1_name);
 
-        it("should read stream forwards", [&test_stream_1_name]() {
-            tpie::file_stream<int> xs;
-            xs.open(test_stream_1_name, tpie::open::read_only);
 
-            AssertThat(xs.can_read(), Is().True());
-            AssertThat(xs.read(), Is().EqualTo(42));
+        describe("File Streams", [&test_stream_1_name]() {
+            it("should read stream forwards", [&test_stream_1_name]() {
+                tpie::file_stream<int> xs;
+                xs.open(test_stream_1_name, tpie::open::read_only);
 
-            AssertThat(xs.can_read(), Is().True());
-            AssertThat(xs.read(), Is().EqualTo(7));
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(42));
 
-            AssertThat(xs.can_read(), Is().True());
-            AssertThat(xs.read(), Is().EqualTo(18));
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(7));
 
-            AssertThat(xs.can_read(), Is().False());
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(18));
+
+                AssertThat(xs.can_read(), Is().False());
+              });
+
+            it("should read stream backwards", [&test_stream_1_name]() {
+                tpie::file_stream<int> xs;
+                xs.open(test_stream_1_name, tpie::open::read_only);
+                xs.seek(0, tpie::file_stream_base::end);
+
+                AssertThat(xs.can_read_back(), Is().True());
+                AssertThat(xs.read_back(), Is().EqualTo(18));
+
+                AssertThat(xs.can_read_back(), Is().True());
+                AssertThat(xs.read_back(), Is().EqualTo(7));
+
+                AssertThat(xs.can_read_back(), Is().True());
+                AssertThat(xs.read_back(), Is().EqualTo(42));
+
+                AssertThat(xs.can_read_back(), Is().False());
+              });
+
+            it("should overwrite entry in the middle of the stream", [&]() {
+                tpie::file_stream<int> xs;
+                xs.open();
+                AssertThat(xs.can_read(), Is().False());
+
+                xs.write(1);
+                xs.write(2);
+                xs.write(3);
+
+                AssertThat(xs.can_read(), Is().False());
+
+                // go back to beginning
+                xs.seek(0);
+
+                AssertThat(xs.read(), Is().EqualTo(1));
+                xs.write(42);
+                AssertThat(xs.read(), Is().EqualTo(3));
+
+                // go back to beginning, and check the change is there
+                xs.seek(0);
+
+                AssertThat(xs.read(), Is().EqualTo(1));
+                AssertThat(xs.read(), Is().EqualTo(42));
+              });
+
+            it("should sort stream", [&test_stream_1_name]() {
+                tpie::file_stream<int> xs;
+                xs.open(test_stream_1_name);
+
+                tpie::progress_indicator_null pi;
+                tpie::sort(xs, [&](const int& a, const int& b) -> bool { return a < b; }, pi);
+
+                AssertThat(xs.read(), Is().EqualTo(7));
+                AssertThat(xs.read(), Is().EqualTo(18));
+                AssertThat(xs.read(), Is().EqualTo(42));
+              });
+
+            // remember that after the sorting test, that the stream now is in sorted order.
+
+            it("should seek prior position into stream", [&test_stream_1_name]() {
+                tpie::file_stream<int> xs;
+                xs.open(test_stream_1_name);
+
+                xs.read(); // Pop 7
+
+                auto position = xs.get_position();
+                xs.read(); // Pop 18
+
+                xs.read(); // Pop 42
+
+                xs.set_position(position);
+                AssertThat(xs.read(), Is().EqualTo(18));
+              });
           });
 
-        it("should read stream backwards", [&test_stream_1_name]() {
-            tpie::file_stream<int> xs;
-            xs.open(test_stream_1_name, tpie::open::read_only);
-            xs.seek(0, tpie::file_stream_base::end);
+        describe("Merge Sort Queue", [&test_stream_1_name]() {
+            it("should sort data", [&]() {
+                // Setup sorter
+                auto pred = [&](const int& a, const int& b) -> bool { return a < b; };
+                tpie::merge_sorter<int, true, decltype(pred)> sorter(pred);
 
-            AssertThat(xs.can_read_back(), Is().True());
-            AssertThat(xs.read_back(), Is().EqualTo(18));
+                sorter.set_available_memory(tpie::get_memory_manager().available());
+                tpie::progress_indicator_null pi;
 
-            AssertThat(xs.can_read_back(), Is().True());
-            AssertThat(xs.read_back(), Is().EqualTo(7));
+                // Start sorter
+                sorter.begin();
 
-            AssertThat(xs.can_read_back(), Is().True());
-            AssertThat(xs.read_back(), Is().EqualTo(42));
+                // Push into sorter
+                sorter.push(42);
+                sorter.push(7);
+                sorter.push(18);
 
-            AssertThat(xs.can_read_back(), Is().False());
+                // Finalize sorter
+                sorter.end();
+                sorter.calc(pi);
+
+                // Pop from sorter
+                AssertThat(sorter.can_pull(), Is().True());
+                AssertThat(sorter.pull(), Is().EqualTo(7));
+                AssertThat(sorter.pull(), Is().EqualTo(18));
+                AssertThat(sorter.pull(), Is().EqualTo(42));
+                AssertThat(sorter.can_pull(), Is().False());
+              });
           });
 
-        it("should sort stream", [&test_stream_1_name]() {
-            tpie::file_stream<int> xs;
-            xs.open(test_stream_1_name);
+        describe("Priority Queue", [&test_stream_1_name]() {
+            it("should sort data on the fly", [&]() {
+                tpie::priority_queue<int> pq;
 
-            tpie::progress_indicator_null pi;
-            tpie::sort(xs, [&](const int& a, const int& b) -> bool { return a < b; }, pi);
+                // Push a little
+                pq.push(42);
+                pq.push(7);
+                pq.push(18);
 
-            AssertThat(xs.read(), Is().EqualTo(7));
-            AssertThat(xs.read(), Is().EqualTo(18));
-            AssertThat(xs.read(), Is().EqualTo(42));
+                // Pop one
+                AssertThat(pq.size(), Is().EqualTo(3));
+                AssertThat(pq.empty(), Is().False());
+
+                AssertThat(pq.top(), Is().EqualTo(7));
+                pq.pop();
+
+                // Push some more
+                pq.push(4);
+
+                // Pop the rest
+                AssertThat(pq.top(), Is().EqualTo(4));
+                pq.pop();
+                AssertThat(pq.top(), Is().EqualTo(18));
+                pq.pop();
+                AssertThat(pq.top(), Is().EqualTo(42));
+                pq.pop();
+              });
           });
 
-        // remember that after the sorting test, that the stream now is in sorted order.
+        describe("Temporary Files", [&]() {
+            tpie::temp_file temporary_file = tpie::temp_file(); // one can parse a persistency boolean if needed
 
-        it("should seek prior position into stream", [&test_stream_1_name]() {
-            tpie::file_stream<int> xs;
-            xs.open(test_stream_1_name);
+            it("should open a (shareable) temporary file", [&]() {
+                tpie::file_stream<int> xs;
+                xs.open(temporary_file);
 
-            xs.read(); // Pop 7
+                xs.write(42);
+              });
 
-            auto position = xs.get_position();
-            xs.read(); // Pop 18
+            it("should open an anonymous temporary files", [&]() {
+                tpie::file_stream<int> xs;
+                xs.open();
+                AssertThat(xs.can_read(), Is().False());
 
-            xs.read(); // Pop 42
+                xs.write(1);
+                xs.write(2);
+                xs.write(3);
 
-            xs.set_position(position);
-            AssertThat(xs.read(), Is().EqualTo(18));
-          });
+                AssertThat(xs.can_read(), Is().False());
 
-        it("should sort data generated on the fly with merge_sorter", [&]() {
-            // Setup sorter
-            auto pred = [&](const int& a, const int& b) -> bool { return a < b; };
-            tpie::merge_sorter<int, true, decltype(pred)> sorter(pred);
+                // go back to beginning
+                xs.seek(0);
 
-            sorter.set_available_memory(tpie::get_memory_manager().available());
-            tpie::progress_indicator_null pi;
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(1));
 
-            // Start sorter
-            sorter.begin();
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(2));
 
-            // Push into sorter
-            sorter.push(42);
-            sorter.push(7);
-            sorter.push(18);
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(3));
 
-            // Finalize sorter
-            sorter.end();
-            sorter.calc(pi);
+                // when going out of scope, xs is killed, and it will be cleaned up
+              });
 
-            // Pop from sorter
-            AssertThat(sorter.can_pull(), Is().True());
-            AssertThat(sorter.pull(), Is().EqualTo(7));
-            AssertThat(sorter.pull(), Is().EqualTo(18));
-            AssertThat(sorter.pull(), Is().EqualTo(42));
-            AssertThat(sorter.can_pull(), Is().False());
-          });
 
-        it("should sort data with priority_queue", [&]() {
-            tpie::priority_queue<int> pq;
+            it("should reopen the (shareable) temporary file", [&]() {
+                tpie::file_stream<int> xs;
+                xs.open(temporary_file);
 
-            // Push a little
-            pq.push(42);
-            pq.push(7);
-            pq.push(18);
-
-            // Pop one
-            AssertThat(pq.size(), Is().EqualTo(3));
-            AssertThat(pq.empty(), Is().False());
-
-            AssertThat(pq.top(), Is().EqualTo(7));
-            pq.pop();
-
-            // Push some more
-            pq.push(4);
-
-            // Pop the rest
-            AssertThat(pq.top(), Is().EqualTo(4));
-            pq.pop();
-            AssertThat(pq.top(), Is().EqualTo(18));
-            pq.pop();
-            AssertThat(pq.top(), Is().EqualTo(42));
-            pq.pop();
+                AssertThat(xs.can_read(), Is().True());
+                AssertThat(xs.read(), Is().EqualTo(42));
+              });
           });
       });
 
