9ead77074186942129150516f32476e79c1ede29
Steffan Sølvsten <201505832@post.au.dk>
2020-07-10 11:20:42 +0200

    Add DOT file output for sink-streams

--- src/coom/dot.h
+++ src/coom/dot.h
@@ -8,6 +8,10 @@
 namespace coom {
   void output_dot(tpie::file_stream<node>& nodes,
                   std::string filename);
+
+  void output_dot(tpie::file_stream<arc>& node_arcs,
+                  tpie::file_stream<arc>& sink_arcs,
+                  std::string filename);
 }
 
 #endif // COOM_DOT_H
