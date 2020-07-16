3a44a358300dbb10c6d1cc0c16f1d83ead29dc98
Steffan Sølvsten <201505832@post.au.dk>
2020-07-16 11:17:16 +0200

    Add unit test for OBDD's that have empty layers

--- test/test_coom_evaluate.cpp
+++ test/test_coom_evaluate.cpp
@@ -135,6 +135,67 @@ go_bandit([]() {
             AssertThat(coom::evaluate(obdd, assignment), Is().True());
           });
 
+        // == CREATE 'SKIPPING' OBDD ==
+        //             START
+        /*
+             1      ---- x0
+            / \
+           /   \    ---- x1
+           |   |
+           2   3    ---- x2
+          / \ / \
+          F T T |   ---- x3
+                |
+                4   ---- x4
+               / \
+               F T
+        */
+
+        tpie::file_stream<node> skip_obdd;
+        skip_obdd.open();
+
+        auto skip_n4 = create_node(4,0, sink_F, sink_T);
+        skip_obdd.write(skip_n4);
+
+        auto skip_n3 = create_node(2,1, sink_T, skip_n4.node_ptr);
+        skip_obdd.write(skip_n3);
+
+        auto skip_n2 = create_node(2,0, sink_F, sink_T);
+        skip_obdd.write(skip_n2);
+
+        auto skip_n1 = create_node(0,0, skip_n2.node_ptr, skip_n3.node_ptr);
+        skip_obdd.write(skip_n1);
+
+        //              END
+        // == CREATE 'SKIPPING' OBDD ==
+
+        it("should be able to evaluate OBDD that skips layers [1]", [&skip_obdd]() {
+            tpie::file_stream<bool> assignment;
+            assignment.open();
+
+            assignment.write(false);
+            assignment.write(true);
+            assignment.write(false);
+            assignment.write(true);
+            assignment.write(true);
+
+            AssertThat(coom::evaluate(skip_obdd, assignment), Is().False());
+          });
+
+        it("should be able to evaluate OBDD that skips layers [2]", [&skip_obdd]() {
+            tpie::file_stream<bool> assignment;
+            assignment.open();
+
+            assignment.write(true);
+            assignment.write(false);
+            assignment.write(true);
+            assignment.write(true);
+            assignment.write(false);
+
+            AssertThat(coom::evaluate(skip_obdd, assignment), Is().False());
+          });
+
+
         it("should return F on F sink-only OBDD", [&]() {
             tpie::file_stream<node> obdd2;
             obdd2.open();
