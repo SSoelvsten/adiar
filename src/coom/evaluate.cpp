415c37ec65a01eba8eeae31248621d449dae5103
BlueKeladry <ablumejakobsen@gmail.com>
2020-07-21 09:10:15 +0200

    Implement evaluate

--- src/coom/evaluate.cpp
+++ src/coom/evaluate.cpp
@@ -11,7 +11,62 @@ namespace coom
   bool evaluate(tpie::file_stream<node> &nodes,
                 tpie::file_stream<bool> &assignment)
   {
-    return false;
+    //reset streams
+    nodes.seek(0, tpie::file_stream_base::end);
+    assignment.seek(0); 
+
+    //få fat i roden
+    node v = nodes.read_back();
+    bool x = assignment.read();
+    int label = label_of(v);
+
+    //check om træet kun er en sink
+    if(is_sink_node(v)) {
+      return value_of(v);
+    }
+
+    //gennemløb af træet
+    while (true) {
+
+      //find det rigtige assignment
+      while(label_of(v) > label) {
+        x = assignment.read();
+        label = label + 1;
+      }
+
+      //check værdien for variablen
+      if(x) {
+
+        //find high for noden og gem
+        uint64_t high = v.high;
+
+        //check om high er en sink og returner
+        if(is_sink(high)) {
+          return value_of(high);
+        }
+        
+        //søg efter high
+        while(v.node_ptr < high) {
+            v = nodes.read_back();
+        }
+      }
+
+      else {
+
+        //som high, bare low - find low for noden og gem
+        uint64_t low = v.low;
+
+        //check om low er en sink og returner
+        if(is_sink(low)) {
+          return value_of(low);
+        }
+        
+        //søg efter low
+        while(v.node_ptr < low) {
+            v = nodes.read_back();
+        }
+      }
+    }
   }
 }
 
