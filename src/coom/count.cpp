f6755bc27b736a80b5999f3a3a4e62815c7eb660
BlueKeladry <ablumejakobsen@gmail.com>
2020-08-13 09:13:57 +0200

    Change use of pow() to bit shift

--- src/coom/count.cpp
+++ src/coom/count.cpp
@@ -43,26 +43,30 @@ namespace coom
     {
       if (sink_pred(root.low))
       {
-        uint64_t new_sum_low = count_skipped_layers ? std::pow(2, biggest_label) : 1;
+        uint64_t new_sum_low = count_skipped_layers ? (biggest_label - label_of(root))<<1 : 1;
+        new_sum_low = new_sum_low == 0 ? 1 : new_sum_low;
         result = result + new_sum_low;
       }
     }
     else
     {
-      uint64_t new_sum_low = count_skipped_layers ? std::pow(2, label_of(root.low) - 1) : 1;
+      uint64_t new_sum_low = count_skipped_layers ? (label_of(root.low) - label_of(root) - 1)<<1 : 1;
+      new_sum_low = new_sum_low == 0 ? 1 : new_sum_low;
       partial_sums.push({root.low, new_sum_low});
     }
     if (is_sink(root.high))
     {
       if (sink_pred(root.high))
       {
-        uint64_t new_sum_high = count_skipped_layers ? std::pow(2, biggest_label) : 1;
+        uint64_t new_sum_high = count_skipped_layers ? (biggest_label - label_of(root))<<1 : 1;
+        new_sum_high = new_sum_high == 0 ? 1 : new_sum_high;
         result = result + new_sum_high;
       }
     }
     else
     {
-      uint64_t new_sum_high = count_skipped_layers ? std::pow(2, label_of(root.high) - 1) : 1;
+      uint64_t new_sum_high = count_skipped_layers ? (label_of(root.high) - label_of(root) - 1)<<1 : 1;
+      new_sum_high = new_sum_high == 0 ? 1 : new_sum_high;
       partial_sums.push({root.high, new_sum_high});
     }
 
@@ -88,13 +92,15 @@ namespace coom
       {
         if (sink_pred(current_node.low))
         {
-          uint64_t new_sum_low = count_skipped_layers ? next_sum * std::pow(2, biggest_label - label_of(current_node)) : next_sum;
+          uint64_t new_sum_low = count_skipped_layers ? next_sum * ((biggest_label - label_of(current_node)) << 1) : next_sum;
+          new_sum_low = new_sum_low == 0 ? next_sum : new_sum_low;
           result = result + new_sum_low;
         }
       }
       else
       {
-        uint64_t new_sum_low = count_skipped_layers ? next_sum * std::pow(2, label_of(current_node.low) - label_of(current_node) - 1) : next_sum;
+        uint64_t new_sum_low = count_skipped_layers ? next_sum * ((label_of(current_node.low) - label_of(current_node) - 1) << 1) : next_sum;
+        new_sum_low = new_sum_low == 0 ? next_sum : new_sum_low;
         partial_sums.push({current_node.low, new_sum_low});
       }
 
@@ -102,13 +108,15 @@ namespace coom
       {
         if (sink_pred(current_node.high))
         {
-          uint64_t new_sum_high = count_skipped_layers ? next_sum * std::pow(2, biggest_label - label_of(current_node)) : next_sum;
+          uint64_t new_sum_high = count_skipped_layers ? next_sum * ((biggest_label - label_of(current_node)) << 1) : next_sum;
+          new_sum_high = new_sum_high == 0 ? next_sum : new_sum_high;
           result = result + new_sum_high;
         }
       }
       else
       {
-        uint64_t new_sum_high = count_skipped_layers ? next_sum * std::pow(2, label_of(current_node.high) - label_of(current_node) - 1) : next_sum;
+        uint64_t new_sum_high = count_skipped_layers ? next_sum * ((label_of(current_node.high) - label_of(current_node) - 1) << 1) : next_sum;
+        new_sum_high = new_sum_high == 0 ? next_sum : new_sum_high;
         partial_sums.push({current_node.high, new_sum_high});
       }
     }
@@ -123,7 +131,7 @@ namespace coom
 
   uint64_t count_paths(tpie::file_stream<node> &nodes)
   {
-    return count_paths(nodes, is_any);
+    return count_paths(nodes, coom::is_any);
   }
 
   uint64_t count_assignments(tpie::file_stream<node> &nodes,
