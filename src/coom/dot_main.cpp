fc375e591a120a012163e30b0e4451cc754b69cf
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2020-08-17 18:32:52 +0200

    Move DOT main function into separate file to allow import elsewhere
    Also closes #24 as it has outlived its purpose

---
+++ src/coom/dot_main.cpp
@@ -0,0 +1,42 @@
+// TODO: Fix header files
+#include "data.cpp"
+#include "dot.cpp"
+
+int main(const int argc, const char* argv[]) {
+  // ===== TPIE =====
+  // Initialize
+  tpie::tpie_init();
+
+  size_t available_memory_mb = 128;
+  tpie::get_memory_manager().set_limit(available_memory_mb*1024*1024);
+
+  // ===== FAIL FAST ON MISSING ARGUMENT =====
+  if (argc == 1) {
+    tpie::log_info() << "Please provide a filename(s) to print" << std::endl;
+    tpie::tpie_finish();
+    exit(1);
+  }
+
+  // ===== OUTPUT DOT FILE =====
+  int arg = 1; // argument 0 is the executable
+
+  while (arg < argc) {
+    const char* filename = argv[arg];
+
+    tpie::file_stream<coom::node> file_stream;
+    file_stream.open(filename, tpie::open::read_only | tpie::open::compression_normal);
+
+    std::string filename_str(filename);
+    coom::output_dot(file_stream, filename_str);
+    file_stream.close();
+
+    arg += 1;
+  }
+
+  // ===== TPIE =====
+  // Close all of TPIE down again
+  tpie::tpie_finish();
+
+  // Return 'all good'
+  exit(0);
+}
