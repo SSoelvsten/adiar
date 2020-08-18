503ee4d2a0e43c2cba459bf3f2ace96fb8832888
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2020-08-18 17:19:51 +0200

    Add is_sink for sink_only OBDDs

---
+++ src/coom/pred.cpp
@@ -0,0 +1,27 @@
+#ifndef COOM_PRED_CPP
+#define COOM_PRED_CPP
+
+#include <stdint.h>
+
+#include <tpie/tpie.h>
+#include <tpie/file_stream.h>
+
+#include "data.h"
+#include "assert.h"
+
+#include "pred.h"
+
+namespace coom {
+  bool is_sink(tpie::file_stream<node>& nodes,
+               const sink_pred& sink_pred = is_any)
+  {
+    assert::is_valid_input_stream(nodes);
+    if (nodes.size() != 1) {
+      return false;
+    }
+    node n = nodes.can_read() ? nodes.read() : nodes.read_back();
+    return is_sink_node(n) && sink_pred(n.node_ptr);
+  }
+}
+
+#endif // COOM_PRED_CPP
---
+++ test/coom/test_pred.cpp
@@ -0,0 +1,83 @@
+#include <coom/pred.cpp>
+
+using namespace coom;
+
+go_bandit([]() {
+    describe("COOM: Predicates", []() {
+        describe("is_sink", []() {
+            it("should accept the correct sink-only OBDD [1]", [&]() {
+                tpie::file_stream<node> sink_T;
+                sink_T.open();
+                sink_T.write(create_sink_node(true));
+
+                AssertThat(is_sink(sink_T, is_true), Is().True());
+              });
+
+            it("should accept the correct sink-only OBDD [2]", [&]() {
+                tpie::file_stream<node> sink_F;
+                sink_F.open();
+                sink_F.write(create_sink_node(false));
+
+                AssertThat(is_sink(sink_F, is_false), Is().True());
+              });
+
+
+            it("should reject the incorrect sink-only OBDD [1]", [&]() {
+                tpie::file_stream<node> sink_T;
+                sink_T.open();
+                sink_T.write(create_sink_node(true));
+
+                AssertThat(is_sink(sink_T, is_false), Is().False());
+              });
+
+            it("should reject the incorrect sink-only OBDD [2]", [&]() {
+                tpie::file_stream<node> sink_F;
+                sink_F.open();
+                sink_F.write(create_sink_node(false));
+
+                AssertThat(is_sink(sink_F, is_true), Is().False());
+              });
+
+            it("should have any sink as default", [&]() {
+                tpie::file_stream<node> sink_T;
+                sink_T.open();
+                sink_T.write(create_sink_node(true));
+
+                AssertThat(is_sink(sink_T), Is().True());
+
+                tpie::file_stream<node> sink_F;
+                sink_F.open();
+                sink_F.write(create_sink_node(false));
+
+                AssertThat(is_sink(sink_F), Is().True());
+              });
+
+            it("should reject the non-sink OBDD [1]", [&]() {
+                tpie::file_stream<node> x0;
+                x0.open();
+                x0.write(create_node(0,MAX_ID, create_sink(false), create_sink(true)));
+
+                AssertThat(is_sink(x0, is_true), Is().False());
+                AssertThat(is_sink(x0, is_false), Is().False());
+                AssertThat(is_sink(x0, is_any), Is().False());
+              });
+
+            it("should reject the non-sink OBDD [2]", [&]() {
+                tpie::file_stream<node> x0_and_x1;
+                x0_and_x1.open();
+
+                x0_and_x1.write(create_node(1, MAX_ID,
+                                     create_sink(false),
+                                     create_sink(true)));
+
+                x0_and_x1.write(create_node(0, MAX_ID,
+                                     create_sink(false),
+                                     create_node_ptr(1, MAX_ID)));
+
+                AssertThat(is_sink(x0_and_x1, is_true), Is().False());
+                AssertThat(is_sink(x0_and_x1, is_false), Is().False());
+                AssertThat(is_sink(x0_and_x1, is_any), Is().False());
+              });
+          });
+      });
+  });
