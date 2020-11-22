8b0a427fea763810170c6eb578e005c4d3dd88fd
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2020-11-22 19:10:43 +0100

    Fix bdd_counter constructs dead nodes for min_lable != 0

--- test/coom/bdd/test_build.cpp
+++ test/coom/bdd/test_build.cpp
@@ -273,6 +273,66 @@ go_bandit([]() {
                 AssertThat(ms.can_pull(), Is().False());
               });
 
+            it("creates counting to 2 in [2,5]", [&]() {
+                bdd res = bdd_counter(2, 5, 2);
+                node_test_stream ns(res);
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(5, 2,
+                                                               sink_T,
+                                                               sink_F)));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(5, 1,
+                                                               sink_F,
+                                                               sink_T)));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(4, 2,
+                                                               create_node_ptr(5,2),
+                                                               sink_F)));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(4, 1,
+                                                               create_node_ptr(5,1),
+                                                               create_node_ptr(5,2))));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(4, 0,
+                                                               sink_F,
+                                                               create_node_ptr(5,1))));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(3, 1,
+                                                               create_node_ptr(4,1),
+                                                               create_node_ptr(4,2))));
+
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(3, 0,
+                                                               create_node_ptr(4,0),
+                                                               create_node_ptr(4,1))));
+                AssertThat(ns.can_pull(), Is().True());
+                AssertThat(ns.pull(), Is().EqualTo(create_node(2, 0,
+                                                               create_node_ptr(3,0),
+                                                               create_node_ptr(3,1))));
+
+                meta_test_stream<node_t, 1> ms(res);
+
+                AssertThat(ms.can_pull(), Is().True());
+                AssertThat(ms.pull(), Is().EqualTo(meta_t { 5 }));
+
+                AssertThat(ms.can_pull(), Is().True());
+                AssertThat(ms.pull(), Is().EqualTo(meta_t { 4 }));
+
+                AssertThat(ms.can_pull(), Is().True());
+                AssertThat(ms.pull(), Is().EqualTo(meta_t { 3 }));
+
+                AssertThat(ms.can_pull(), Is().True());
+                AssertThat(ms.pull(), Is().EqualTo(meta_t { 2 }));
+
+                AssertThat(ms.can_pull(), Is().False());
+              });
+
             it("creates counting to 3 in [0,8]", [&]() {
                 bdd res = bdd_counter(0, 8, 3);
                 node_test_stream ns(res);
