c7f04a76410dda58ade69780ddbb7da6332fd804
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2020-11-22 12:10:13 +0100

    Generalize naming of tuple functions

--- src/coom/tuple.h
+++ src/coom/tuple.h
@@ -10,25 +10,14 @@ namespace coom {
     ptr_t t2;
   };
 
-  struct tuple_data
+  struct tuple_data : tuple
   {
-    ptr_t t1;
-    ptr_t t2;
     ptr_t data_low;
     ptr_t data_high;
   };
 
   //////////////////////////////////////////////////////////////////////////////
   // Priority queue functions
-  struct tuple_queue_lt
-  {
-    bool operator()(const tuple &a, const tuple &b)
-    {
-      return std::min(a.t1, a.t2) < std::min(b.t1, b.t2) ||
-            (std::min(a.t1, a.t2) == std::min(b.t1, b.t2) && std::max(a.t1, a.t2) < std::max(b.t1, b.t2));
-    }
-  };
-
   struct tuple_queue_label
   {
     label_t label_of(const tuple &t)
@@ -37,9 +26,18 @@ namespace coom {
     }
   };
 
-  struct tuple_queue_data_lt
+  struct tuple_queue_1_lt
   {
-    bool operator()(const tuple_data &a, const tuple_data &b)
+    bool operator()(const tuple &a, const tuple &b)
+    {
+      return std::min(a.t1, a.t2) < std::min(b.t1, b.t2) ||
+        (std::min(a.t1, a.t2) == std::min(b.t1, b.t2) && std::max(a.t1, a.t2) < std::max(b.t1, b.t2));
+    }
+  };
+
+  struct tuple_queue_2_lt
+  {
+    bool operator()(const tuple &a, const tuple &b)
     {
       return std::max(a.t1, a.t2) < std::max(b.t1, b.t2) ||
             (std::max(a.t1, a.t2) == std::max(b.t1, b.t2) && std::min(a.t1, a.t2) < std::min(b.t1, b.t2));
