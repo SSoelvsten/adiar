251d82c20ac31559ad1d565ef2a0b844b808c522
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2020-12-11 17:36:32 +0100

    Make temporary file directory customizable

--- src/coom/coom.cpp
+++ src/coom/coom.cpp
@@ -3,12 +3,26 @@
 
 #include "coom.h"
 
+#include <tpie/tempname.h>
+
 namespace coom
 {
   void coom_init(size_t memory_limit_mb)
   {
     tpie::tpie_init();
+
+    // Memory usage
     set_limit(memory_limit_mb);
+
+    // Temporary files
+    tpie::tempname::set_default_base_name("COOM");
+    tpie::tempname::set_default_extension("coom"); // does this do anything?
+  }
+
+  void coom_init(size_t memory_limit_mb, std::string temp_dir)
+  {
+    coom_init(memory_limit_mb);
+    tpie::tempname::set_default_path(temp_dir);
   }
 
   void set_limit(size_t memory_limit_mb)
