e91b2ab379ba1abd14894068ae745fafe0693564
Steffan 'Ikslorin' Soelvsten <201505832@post.au.dk>
2021-05-21 10:22:50 +0200

    Add Reduction Rule as a providable strategy

--- src/adiar/internal/reduce.h
+++ src/adiar/internal/reduce.h
@@ -6,13 +6,26 @@
 namespace adiar
 {
   //////////////////////////////////////////////////////////////////////////////
+  /// \brief Applies Reduction Rule 1 to a node (should the node be omitted or
+  ///        not?)
+  ///
+  /// \return Returns NIL if the node should be kept as-is. Otherwise, the node
+  ///         should be replaced with the returned value.
+  //////////////////////////////////////////////////////////////////////////////
+  typedef std::function<ptr_t(const node_t&)> reduction_rule_t;
+
+  extern const reduction_rule_t reduction_rule_bdd;
+
+  //////////////////////////////////////////////////////////////////////////////
   /// \brief Reduce a given edge-based BDD.
   ///
   /// \param arc_file The unreduced bdd in its arc-based representation
+  /// \param reduction_rule The reduction rule to apply on nodes
   ///
-  /// \return The reduced bdd in a node-based representation
+  /// \return The reduced decision diagram in a node-based representation
   //////////////////////////////////////////////////////////////////////////////
-  node_file reduce(const arc_file &arc_file);
+  node_file reduce(const arc_file &arc_file,
+                   const reduction_rule_t& reduction_rule);
 }
 
 #endif // ADIAR_REDUCE_H
