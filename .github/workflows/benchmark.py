851186862958e09502f918e6ac895b95a67816fa
Steffan Sølvsten <201505832@post.au.dk>
2021-12-11 14:35:10 +0100

    Fix benchmarking action also works for forks

--- .github/workflows/benchmark.py
+++ .github/workflows/benchmark.py
@@ -1,50 +1,118 @@
 import re
+import io
+import selectors
 import subprocess
 import sys
 
+
+# ------------------------------------------------------------------------------
+# From 'nawatts/capture-and-print-subprocess-output.py'
+
+def capture_subprocess_output(subprocess_args):
+    # Start subprocess
+    # bufsize = 1 means output is line buffered
+    # universal_newlines = True is required for line buffering
+    process = subprocess.Popen(subprocess_args,
+                               bufsize=1,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               universal_newlines=True)
+
+    # Create callback function for process output
+    buf = io.StringIO()
+    def handle_output(stream, mask):
+        # Because the process' output is line buffered, there's only ever one
+        # line to read when this function is called
+        line = stream.readline()
+        buf.write(line)
+        sys.stdout.write(line)
+
+    # Register callback for an "available for read" event from subprocess' stdout stream
+    selector = selectors.DefaultSelector()
+    selector.register(process.stdout, selectors.EVENT_READ, handle_output)
+
+    # Loop until subprocess is terminated
+    while process.poll() is None:
+        # Wait for events and handle them with their registered callbacks
+        events = selector.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+    # Get process return code
+    return_code = process.wait()
+    selector.close()
+
+    success = (return_code == 0)
+
+    # Store buffered output
+    output = buf.getvalue()
+    buf.close()
+
+    return (success, output)
+
+
 # ------------------------------------------------------------------------------
 # Branch switching
-def git_checkout(branch_name):
-    #print('git', 'checkout', branch_name)
-    subprocess.run(['git', 'checkout', branch_name], capture_output=True)
-    return
+def remote_name(remote):
+    return remote.replace('/','_')
+
+
+def git_remove_remote(remote):
+    if (remote == 'origin'):
+        return # origin should not be purged
+
+    subprocess.run(['git', 'remote', 'remove', remote_name(remote)])
+
+
+def git_add_remote(remote):
+    if (remote == 'origin'):
+        return # origin hopefully exists
+
+    git_remove_remote(remote)
+    subprocess.run(['git', 'remote', 'add', remote_name(remote), f'https://github.com/{remote}.git'])
+
+
+def git_fetch():
+    subprocess.run(['git', 'fetch', '--all'])
+
+
+def git_checkout(remote, branch):
+    subprocess.run(['git', 'checkout', f'{remote_name(remote)}/{branch}'])
+    subprocess.run(['git', 'status'])
 
 # ------------------------------------------------------------------------------
 # Running benchmarks
 QUEENS_N = 14
 
 def run_queens():
-    out = subprocess.run(['make', 'example/queens', 'N='+str(QUEENS_N), 'M=8096'],
-                         capture_output=True,
-                         text=True)
-
-    # The output ends on the error due to the CMake warnings during build
-    out_txt = out.stderr
+    out_txt = capture_subprocess_output(['make', 'example/queens', 'N='+str(QUEENS_N), 'M=8096'])[1]
 
     matches = re.findall("time:\s*([0-9\.]+)\s*s", out_txt)
     timing = sum([float(t) for t in matches])
     return timing
 
+
 # ------------------------------------------------------------------------------
 # Markdown helper functions
-def markdown_table(timings):
-    keys = list(timings.keys())
-
-    header = ' | '.join(keys)
-    line   = '-|-'.join([re.sub('.','-',b) for b in keys])
+def markdown_table(args, timings):
+    header = ' | '.join([f'{r}/{b}' for (r,b) in args])
+    line   = '-|-'.join([re.sub('.','-',f'{r}/{b}') for (r,b) in args])
 
     # assumes every value is a list of the same length
-    number_of_rows = len(timings[keys[0]])
+    number_of_rows = len(timings[args[0][0]][args[0][1]])
     rows = []
 
     for r in range(0, number_of_rows):
         rows.append(' | '.join([t + ' ' * (len(b)-len(str(t)))
-                                  for (b, t) in [(b, str(timings[b][r])) for b in keys]]))
+                                for (b, t) in [(f'{r}/{b}', str(timings[r][b])) for (r,b) in args]]))
 
     return '\n'.join(['| '+ header +' |', '|-'+ line +'-|'] + ['| '+ r +' |' for r in rows])
 
+
 def bold(txt):
     return "**"+txt+"**"
+
 
 def spoiler(txt, summary):
     return '> ' + ('<details>\n' +
@@ -52,31 +120,42 @@ def spoiler(txt, summary):
                    + txt + '\n' +
                    '</details>').replace('\n', '\n> ')
 
-def performance_report(branches, timings):
-    output_txt = (f'# Benchmark Report `{branches[0]}`\n' +
-                  f'Minimum running time for {QUEENS_N}-Queens: {min(timings[branches[0]])}s\n\n' +
-                  spoiler(f'Running times (s) for {QUEENS_N}-Queens:\n' + ', '.join([str(t) for t in timings[branches[0]]]), 'Raw Data'))
+
+def performance_report(args, timings):
+    output_txt = (f'# Benchmark Report `{args[0]}/{args[1]}`\n' +
+                  f'Minimum running time for {QUEENS_N}-Queens: {min(timings[args[0][1]])}s\n\n' +
+                  spoiler(f'Running times (s) for {QUEENS_N}-Queens:\n' +
+                          ', '.join([str(t) for t in timings[args[0][1]]]), 'Raw Data'))
 
     return (0, output_txt)
 
-def comparison_report(branches, timings):
+
+def comparison_report(args, timings):
     # Create table of raw data
-    raw_data_txt = spoiler(f'Running times (s) for {QUEENS_N}-Queens:\n' + markdown_table(timings), 'Raw Data')
+    raw_data_txt = spoiler(f'Running times (s) for {QUEENS_N}-Queens:\n' + markdown_table(args, timings),
+                           'Raw Data')
 
     # Compute minimum
-    for k in timings.keys():
-        timings[k] = [min(timings[k])]
+    for (r,b) in args:
+        timings[r][b] = [min(timings[r][b])]
 
     minimum_data_txt = (f'Minimum running time (s) for {QUEENS_N}-Queens:\n' +
-                        markdown_table(timings) + '\n')
+                        markdown_table(args, timings) + '\n')
 
     output_txt = ''
 
     # Compute differences
-    reference_t = min(timings[branches[0]])
-    tested_t = [(b, min(timings[b])) for b in branches[1::]]
+    reference_t = min(timings[args[0][0]][args[0][1]])
+    tested_t = [(b, min(timings[b[0]][b[1]])) for b in args[1::]]
 
-    diffs = [(b, (t - reference_t) / reference_t) for (b,t) in tested_t]
+    diffs = []
+
+    if reference_t > 0.0:
+        diffs = [(b, (t - reference_t) / reference_t)
+                 for (b,t) in tested_t]
+    else:
+        diffs = [(b, float('inf') if t > 0 else float(0))
+                 for (b,t) in tested_t]
 
     # Check if any diffs violate the threshold
     worst_diff = 0.0 if len(diffs) == 0 else max([t for (b,t) in diffs])
@@ -85,15 +164,18 @@ def comparison_report(branches, timings):
 
     diffs_txt = [f'`{b}` ' + ('does not impact performance'
                               if t == 0.0
-                              else ('is '+('an improvement' if t < 0.0 else 'a regression')+f' of {abs(t)*100.0:.2f}%'))
+                              else ('is '+('an improvement' if t < 0.0 else 'a regression')+
+                                    f' of {abs(t)*100.0:.2f}%'))
                  for (b,t) in diffs]
 
     output_txt = (f'# Benchmark Report :{report_color}_circle:\n' +
-                  bold(' and '.join(diffs_txt) + ' (compared to `'+branches[0]+'`).') + '\n\n' +
+                  bold(' and '.join(diffs_txt) + f' (compared to `{args[0][0]}/{args[0][1]}`).') +
+                  '\n\n' +
                   minimum_data_txt + '\n' +
                   raw_data_txt)
 
     return (exit_code, output_txt)
+
 
 # ------------------------------------------------------------------------------
 # Main
@@ -112,23 +194,43 @@ if __name__ == '__main__':
         print('\n  time: ', min(timings), ' s')
 
     elif len(sys.argv) > 1:
-        branches = [b for b in sys.argv[1::]]
-        timings = { }
+        if sys.argv == 2:
+            sys.argv = [sys.argv[0], 'origin', sys.argv[1]]
 
-        for branch in branches:
-            timings[branch] = []
+        if len(sys.argv) % 2 != 1:
+            print('Please provide pairs of remote , branch')
+            exit(-1)
+
+        args = list(zip(*[iter(sys.argv[1::])]*2))
+
+        for remote in list(set([r for (r,b) in args])):
+            git_add_remote(remote)
+
+        git_fetch()
+
+        # Initialise results data
+        timings  = {  }
+
+        for (remote, branch) in args:
+            timings[remote] = {}
+
+        for (remote, branch) in args:
+            timings[remote][branch] = []
 
         # Run benchmarks on all branches as fairly as possible
         for run in range(0, REPETITIONS):
-            for branch in branches:
-                git_checkout(branch)
-                timings[branch].append(run_queens())
+            for (remote, branch) in args:
+                git_checkout(remote, branch)
+                timings[remote][branch].append(run_queens())
+
+        for remote in list(set([r for (r,b) in args])):
+            git_remove_remote(remote)
 
         exit_code = 0
         with open('benchmark.out', 'w') as file:
-            report = (comparison_report(branches, timings)
-                      if len(branches) > 1
-                      else performance_report(branches, timings))
+            report = (comparison_report(args, timings)
+                      if len(args) > 1
+                      else performance_report(args, timings))
 
             exit_code = report[0]
 
