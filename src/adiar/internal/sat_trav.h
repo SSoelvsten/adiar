f2d7af5e147fe715d5703120608a91df8b00bcf8
Steffan Sølvsten <soelvsten@cs.au.dk>
2021-12-25 10:16:06 +0100

    Change assignment_find into a reusable internal policied function

---
+++ src/adiar/internal/sat_trav.h
@@ -0,0 +1,38 @@
+#ifndef ADIAR_INTERNAL_SAT_TRAV_H
+#define ADIAR_INTERNAL_SAT_TRAV_H
+
+#include <adiar/data.h>
+
+#include <adiar/file.h>
+#include <adiar/file_stream.h>
+
+namespace adiar
+{
+  typedef std::function<void(label_t, bool)> sat_trav_callback_t;
+
+  template<typename sat_trav_policy>
+  void sat_trav(const typename sat_trav_policy::reduced_t &dd,
+                const sat_trav_callback_t &callback)
+  {
+    // TODO: unit test sink
+
+    node_stream<> in_nodes(dd);
+    node_t n_curr = in_nodes.pull();
+    ptr_t  n_next = n_curr.uid;
+
+    while (!is_sink(n_next)) {
+      // forward to n_next
+      while (n_curr.uid < n_next) { n_curr = in_nodes.pull(); }
+
+      adiar_debug(n_curr.uid == n_next,
+                  "Invalid uid chasing; fell out of Decision Diagram");
+
+      const bool go_high = sat_trav_policy::go_high(n_curr);
+      callback(label_of(n_curr), go_high);
+
+      n_next = go_high ? n_curr.high : n_curr.low;
+    }
+  }
+}
+
+#endif // ADIAR_INTERNAL_SAT_TRAV_H
