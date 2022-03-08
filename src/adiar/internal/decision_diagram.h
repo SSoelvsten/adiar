05c9afe1f7e156eec335242ef12eedd10e6feedf
Anna Blume <ablumejakobsen@gmail.com>
2022-03-08 18:42:48 +0100

    Make if_then_else use internal memory when possible

--- src/adiar/internal/decision_diagram.h
+++ src/adiar/internal/decision_diagram.h
@@ -113,6 +113,8 @@ namespace adiar
     friend size_t __prod_size_based_upper_bound(const typename prod_policy::reduced_t &in_1,
                                                 const typename prod_policy::reduced_t &in_2);
 
+    friend size_t __ite_size_based_upper_bound(const decision_diagram &in_if, const decision_diagram &in_then, const decision_diagram &in_else);
+
     friend label_t min_label(const decision_diagram &dd);
     friend label_t max_label(const decision_diagram &dd);
 
