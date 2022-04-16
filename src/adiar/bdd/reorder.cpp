27917152eb7cec044b7220c0fb4de482a31bc721
Anders Benjamin Clausen <anbclausen@gmail.com>
2022-04-16 17:32:02 +0200

    Implemented reverse path algorithm, but unable to test

--- src/adiar/bdd/reorder.cpp
+++ src/adiar/bdd/reorder.cpp
@@ -17,4 +17,29 @@ namespace adiar
   {
     return bdd_sink(false);
   }
+
+  struct assignment
+  {
+    label_t label;
+    bool assignment;
+  };
+
+  std::vector<assignment> reverse_path(const arc_file &af, node_t &n)
+  {
+    std::vector<assignment> assignments = std::vector<assignment>();
+    adiar::node_arc_stream<> fs(af);
+    ptr_t current = high_arc_of(n).source;
+    
+    while (fs.can_pull())
+    {
+      arc_t a = fs.pull();
+      if (a.target == current) {
+        current = a.source;
+        assignments.push_back(assignment{label_of(current), is_high(a)});
+      }
+    }
+
+    fs.detach();
+    return assignments;
+  }
 }
