c4b356b7ddccc0d1c2511c442f89ce3a4afd97db
Anders Benjamin Clausen <anbclausen@gmail.com>
2022-04-22 13:24:06 +0200

    Push children done - no idea if it works

--- src/adiar/bdd/reorder.cpp
+++ src/adiar/bdd/reorder.cpp
@@ -31,8 +31,8 @@ namespace adiar
   __bdd bdd_reorder(const bdd &dd)
   {
     // prøv levelized_priority_queue for UNLIMITED POWER
-    external_priority_queue<reorder_request, reorder_lt> pq(memory::available(), 0); //0 is pq external doesnt care
-    
+    external_priority_queue<reorder_request, reorder_lt> pq(memory::available(), 0); // 0 is pq external doesnt care
+
     /*
     Sådan pusher vi arcs (substitute.h i internal/substitution.h))):
     while(substitute_pq.can_pull() && substitute_pq.top().target == n_res.uid) {
@@ -47,12 +47,39 @@ namespace adiar
     return bdd_sink(false);
   }
 
+  void push_children(external_priority_queue<reorder_request, reorder_lt> &pq, const ptr_t source, arc_file &af, const bdd &f)
+  {
+    auto deal_with = [&](bool b)
+    {
+      assignment_file path = reverse_path(af, source);
+      assignment_writer aw(path);
+      aw.unsafe_push(assignment_t{label_of(source), b});
+
+      bdd f_ikb = bdd_restrict(f, path);
+      label_t label = min_label(f_ikb);
+
+      bool is_leaf = label == -1;
+      if (!is_leaf)
+      {
+        pq.push(reorder_request{source, label});
+      }
+      else
+      {
+        arc_writer aw(af);
+        aw.unsafe_push(arc_t{source, create_sink_ptr(value_of(f_ikb))});
+      }
+    };
+
+    deal_with(true);
+    deal_with(false);
+  }
+
   // N/B I/Os
   label_t min_label(const bdd &dd)
   {
     if (is_sink(dd))
     {
-      return 0; // TODO: nodes are indexed from 0 - change
+      return -1; // TODO: nodes are indexed from 0 - change
     }
 
     label_t result = UINT_MAX;
@@ -78,11 +105,11 @@ namespace adiar
   {
     assignment_file ass_file;
     assignment_writer aw(ass_file);
-    
+
     // TODO: This node_arc_stream should read backwards as our reorder implementation
     //       will always write the arc of the root first in the file.
     adiar::node_arc_stream<false> fs(af); // true means that we read it backwards
-    
+
     // Måske virker node_arc_stream ikke.. (Detach er ikke defineret)
     // Denne node_arc_stream er ikke beregnet til at at read, write, read, write
     // Vi tror, det er muligt, men måske render vi ind i problemer senere.
