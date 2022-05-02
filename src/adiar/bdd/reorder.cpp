8a8b161ae80de41473d08b74321ef0be950bb518
Kent Nielsen <kentnielsen26@gmail.com>
2022-05-02 10:37:36 +0200

    Added pre-processor tags for debug printing

--- src/adiar/bdd/reorder.cpp
+++ src/adiar/bdd/reorder.cpp
@@ -10,6 +10,8 @@
 #include <adiar/internal/substitution.h>
 #include <adiar/internal/util.h>
 
+#define PRINT 0
+
 namespace adiar
 {
   std::vector<label_t> perm;
@@ -17,17 +19,26 @@ namespace adiar
 
   struct reorder_request
   {
-    ptr_t source; // gemmer også is_high (brug flag, unflag, is_flagged)
+    ptr_t source;
     label_t child_level;
   };
 
   struct reorder_lt
   {
-    bool operator()(const reorder_request &a, const reorder_request &b) const
+    bool operator()(const reorder_request &arc, const reorder_request &b) const
     {
-      return a.child_level < b.child_level;
+      return arc.child_level < b.child_level;
     }
   };
+
+  void debug_log(const std::string &msg, int tabs)
+  {
+#if PRINT
+    for (int i = 0; i < tabs; i++)
+      std::cout << "  ";
+    std::cout << msg << std::endl;
+#endif
+  }
 
   // T/B I/Os
   // TODO reverse path of two arc at once
@@ -44,16 +55,16 @@ namespace adiar
       // Vi tror, det er muligt, men måske render vi ind i problemer senere.
       ptr_t current = n;
 
-      std::cout << "REVERSE PATH: Starting arc: " << n << std::endl;
+      debug_log("REVERSE PATH: Starting arc: " + std::to_string(n), 2);
 
       while (fs.can_pull())
       {
-        arc_t a = fs.pull();
-        std::cout << "REVERSE PATH: Looking @ source: " << a.source << " --" << is_flagged(a.source) << "--> target: " << a.target << std::endl;
-        if (a.target == current)
+        arc_t arc = fs.pull();
+        debug_log("REVERSE PATH: Looking @ source: " + std::to_string(arc.source) + " --" + std::to_string(is_flagged(arc.source)) + "--> target: " + std::to_string(arc.target), 2);
+        if (arc.target == current)
         {
-          current = unflag(a.source);
-          aw.unsafe_push(assignment{perm[label_of(current)], is_high(a)});
+          current = unflag(arc.source);
+          aw.unsafe_push(assignment{perm[label_of(current)], is_high(arc)});
         }
       }
       af.make_writeable();
@@ -81,32 +92,35 @@ namespace adiar
     return reverse_path(af, n, ass_of_n, true);
   }
 
-  std::tuple<assignment_file, assignment_file> dual_reverse_path(const arc_file &af, ptr_t n, ptr_t i)
+  std::tuple<assignment_file, assignment_file> dual_reverse_path(const arc_file &af, ptr_t n, ptr_t m)
   {
     assignment_file ass_file_n;
-    assignment_file ass_file_i;
+    assignment_file ass_file_m;
 
     {
       assignment_writer aw_n(ass_file_n);
-      assignment_writer aw_i(ass_file_i);
+      assignment_writer aw_m(ass_file_m);
 
       adiar::node_arc_stream<> fs(af);
 
       ptr_t current_n = n;
-      ptr_t current_i = i;
+      ptr_t current_m = m;
+
+      debug_log("DUAL REVERSE PATH: Starting arc_n: " + std::to_string(n) + " arc_m: " + std::to_string(m), 2);
 
       while (fs.can_pull())
       {
         arc_t arc = fs.pull();
+        debug_log("DUAL REVERSE PATH: Looking @ source: " + std::to_string(arc.source) + " --" + std::to_string(is_flagged(arc.source)) + "--> target: " + std::to_string(arc.target), 2);
         if (arc.target == current_n)
         {
           current_n = unflag(arc.source);
           aw_n.unsafe_push(assignment{perm[label_of(current_n)], is_high(arc)});
         }
-        if (arc.target == current_i)
+        if (arc.target == current_m)
         {
-          current_i = unflag(arc.source);
-          aw_i.unsafe_push(assignment{perm[label_of(current_i)], is_high(arc)});
+          current_m = unflag(arc.source);
+          aw_m.unsafe_push(assignment{perm[label_of(current_m)], is_high(arc)});
         }
       }
       af.make_writeable();
@@ -114,9 +128,9 @@ namespace adiar
 
     simple_file_sorter<assignment_t> sf_sorter;
     sf_sorter.sort(ass_file_n);
-    sf_sorter.sort(ass_file_i);
+    sf_sorter.sort(ass_file_m);
 
-    return {ass_file_n, ass_file_i};
+    return {ass_file_n, ass_file_m};
   }
 
   // N/B I/Os
@@ -151,34 +165,36 @@ namespace adiar
     auto deal_with = [&](bool b)
     {
       assignment_file path = reverse_path(af, source, assignment_t{perm[label_of(source)], b});
-      std::cout << "PUSH-CHILDREN: reverse path done" << std::endl;
+      debug_log("PUSH-CHILDREN: reverse path done", 1);
+#if PRINT
       {
-        std::cout << "PUSH-CHILDREN: reverse_path Assignment = (";
+        std::cout << "  PUSH-CHILDREN: reverse_path Assignment = (";
         assignment_stream<> as(path);
         while (as.can_pull())
         {
-          assignment_t a = as.pull();
-          std::cout << a.label << " = " << a.value << ", ";
+          assignment_t arc = as.pull();
+          std::cout << arc.label << " = " << arc.value << ", ";
         }
         std::cout << ")" << std::endl;
       }
+#endif
 
       bdd f_ikb = bdd_restrict(f, path);
-      std::cout << "PUSH-CHILDREN: restriction done" << std::endl;
+      debug_log("PUSH-CHILDREN: restriction done", 1);
       label_t label = min_label(f_ikb);
-      std::cout << "PUSH-CHILDREN: min-label found: " << label << std::endl;
+      debug_log("PUSH-CHILDREN: min-label done", 1);
 
       bool is_leaf = label == 0xffffffff;
       if (!is_leaf)
       {
-        std::cout << "PUSH-CHILDREN: pushing non-leaf" << std::endl;
+        debug_log("PUSH-CHILDREN: pushing non-leaf", 1);
         ptr_t src;
         if (b)
           src = flag(source);
         else
           src = unflag(source);
         pq.push(reorder_request{src, label});
-        std::cout << "RR: {" << src << " , " << label << "}" << std::endl;
+        debug_log("RR: {" + std::to_string(src) + ", " + std::to_string(label) + "}", 1);
       }
       else
       {
@@ -187,19 +203,17 @@ namespace adiar
           src = flag(source);
         else
           src = unflag(source);
-        std::cout << "PUSH-CHILDREN: writing arc to leaf" << std::endl;
+        debug_log("PUSH-CHILDREN: writing arc to leaf", 1);
         arc_writer aw(af);
         aw.unsafe_push(arc_t{src, create_sink_ptr(value_of(f_ikb))});
       }
     };
 
-    std::cout << "PUSH-CHILDREN: Called" << std::endl;
-
     deal_with(false);
     deal_with(true);
   }
 
-  void initPermutation(const std::vector<label_t> permutation)
+  void init_permutation(const std::vector<label_t> permutation)
   {
     perm = permutation;
     perm_inv = std::vector<label_t>(permutation.size(), 0);
@@ -211,9 +225,9 @@ namespace adiar
 
   node_file convert_arc_file_to_node_file(const arc_file &af)
   {
-    simple_file<arc_t> allarcs;
+    simple_file<arc_t> all_arcs;
     {
-      simple_file_writer<arc_t> sfw(allarcs);
+      simple_file_writer<arc_t> sfw(all_arcs);
 
       node_arc_stream<true> as(af);
       while (as.can_pull())
@@ -230,23 +244,20 @@ namespace adiar
       }
     }
 
-    auto pred_arc = [](const arc_t &a, const arc_t &b) -> bool
-    { return b.source < a.source; };
+    auto pred_arc = [](const arc_t &arc, const arc_t &b) -> bool
+    { return b.source < arc.source; };
     simple_file_sorter<arc_t, decltype(pred_arc)> sfs;
-    sfs.sort(allarcs, pred_arc);
+    sfs.sort(all_arcs, pred_arc);
 
     node_file nodes;
     node_writer nw(nodes);
 
-    file_stream<arc_t> fs(allarcs);
+    file_stream<arc_t> fs(all_arcs);
     while (fs.can_pull())
     {
       arc_t arc1 = fs.pull();
       arc_t arc2 = fs.pull();
-      std::cout << "Arc: " << label_of(arc1.source) << "," << id_of(arc1.source) << "--" << is_flagged(arc1.source) << "--> " << arc1.target << std::endl;
-      std::cout << "Arc: " << label_of(arc2.source) << "," << id_of(arc2.source) << "--" << is_flagged(arc2.source) << "--> " << arc2.target << std::endl;
       node n = node_of(arc2, arc1);
-      std::cout << n.uid << std::endl;
       nw.push(n);
     }
     return nodes;
@@ -254,30 +265,29 @@ namespace adiar
 
   __bdd bdd_reorder(const bdd &dd, const std::vector<label_t> permutation)
   {
-    initPermutation(permutation);
+    init_permutation(permutation);
 
-    std::cout << "Reorder started" << std::endl;
+    debug_log("Reorder started", 0);
 
     // prøv levelized_priority_queue for UNLIMITED POWER
     external_priority_queue<reorder_request, reorder_lt> pq(memory::available() / 2, 0); // 0 is pq external doesnt care
 
     arc_file af;
     ptr_t root = create_node_ptr(0, 0);
-    std::cout << "Now pushing children" << std::endl;
     push_children(pq, root, af, dd);
 
-    std::cout << "Initialization done" << std::endl;
+    debug_log("Initialization done", 0);
 
     while (!pq.empty())
     {
-      std::cout << "PQ loop" << std::endl;
+      debug_log("PQ loop", 0);
 
       // TODO https://github.com/Mortal/tpieex/blob/master/main.cc line 68-70
       // TODO add operator as argument in the constructor.
-      auto pred = [&](const reorder_request &a, const reorder_request &b) -> bool
+      auto pred = [&](const reorder_request &arc, const reorder_request &b) -> bool
       {
         assignment_file path_a, path_b;
-        std::tie(path_a, path_b) = dual_reverse_path(af, a.source, b.source);
+        std::tie(path_a, path_b) = dual_reverse_path(af, arc.source, b.source);
 
         bdd a_restrict = bdd_restrict(dd, path_a);
 
@@ -313,7 +323,6 @@ namespace adiar
       reorder_request rr = pq.top();
       pq.pop();
       m_sorter.push(rr);
-      std::cout << "First element from PQ removed" << std::endl;
 
       while (!pq.empty() && pq.top().child_level == rr.child_level)
       {
@@ -324,26 +333,26 @@ namespace adiar
       m_sorter.end();
       m_sorter.calc(d_indicator);
 
-      // The root of output can never be within a restriction
+      // The root of output can never be within arc restriction
       // Therefore r is assigned this variable, to ensure that
       // the following equality check will always fail in the first iteration!
-      // ie. this is a way of setting r to null
+      // ie. this is arc way of setting r to null
       bdd r = bdd_ithvar(perm[0]);
       bdd r_prime;
       uint64_t i = -1;
       while (m_sorter.can_pull())
       {
-        std::cout << "Merger loop" << std::endl;
+        debug_log("Merger loop", 0);
 
         reorder_request m_rr = m_sorter.pull();
         assignment_file path = reverse_path(af, m_rr.source, assignment{perm[label_of(m_rr.source)], is_flagged(m_rr.source)});
 
         r_prime = bdd_restrict(dd, path);
-        std::cout << "R_Prime restriction found" << std::endl;
+        debug_log("R_Prime restriction found", 0);
 
         if (bdd_equal(r, r_prime))
         {
-          std::cout << "R and R_Prime equal" << std::endl;
+          debug_log("R and R_Prime equal", 0);
           ptr_t old_node = create_node_ptr(m_rr.child_level, i);
           {
             arc_writer aw(af);
@@ -353,7 +362,7 @@ namespace adiar
         else
         {
           i++;
-          std::cout << "R and R_Prime NOT equal" << std::endl;
+          debug_log("R and R_Prime NOT equal", 0);
           ptr_t new_node = create_node_ptr(m_rr.child_level, i);
           {
             arc_writer aw(af);
@@ -364,13 +373,8 @@ namespace adiar
         r = r_prime;
       }
     }
-
-    std::cout << "Finished reordering BDD" << std::endl;
-
+    debug_log("Reorder done", 0);
     node_file nodes = convert_arc_file_to_node_file(af);
-
-    std::cout << "Returning" << std::endl;
     return __bdd(nodes);
   }
-
 }
