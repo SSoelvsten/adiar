9be04f9ce14ebdcc346cf86eaa13b7ea44aa43ea
Anders Benjamin Clausen <anbclausen@gmail.com>
2022-05-03 22:46:43 +0200

    Put in full size exponential size bdd

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include <adiar/adiar.h>
 #include <adiar/internal/dot.h>
 
-#define EXP_BDD 0
-#define OUR_BDD 1
+#define EXP_BDD 1
+#define OUR_BDD 0
 
 using namespace adiar;
 
@@ -39,26 +39,34 @@ int main(int argc, char *argv[])
     bdd x1 = bdd_ithvar(1);
     bdd x2 = bdd_ithvar(2);
     bdd x3 = bdd_ithvar(3);
+    bdd x4 = bdd_ithvar(4);
+    bdd x5 = bdd_ithvar(5);
+    bdd x6 = bdd_ithvar(6);
+    bdd x7 = bdd_ithvar(7);
     bdd x0Ax1 = bdd_and(x0, x1);
     bdd x2Ax3 = bdd_and(x2, x3);
-    bdd root = bdd_or(x0Ax1, x2Ax3);
+    bdd x4Ax5 = bdd_and(x4, x5);
+    bdd x6Ax7 = bdd_and(x6, x7);
+    bdd int1 = bdd_or(x0Ax1, x2Ax3);
+    bdd int2 = bdd_or(x4Ax5, x6Ax7);
+    bdd root = bdd_or(int1, int2);
 
-    std::vector<label_t> permutation = {0, 3, 1, 2};
+    std::vector<label_t> permutation = {0, 2, 4, 6, 1, 3, 5, 7};
     bdd new_order = bdd_reorder(root, permutation);
-    std::vector<label_t> permutation_inverse = {0, 2, 3, 2};
+    std::vector<label_t> permutation_inverse = {0, 4, 1, 5, 2, 6, 3, 7};
     bdd org_back = bdd_reorder(new_order, permutation_inverse);
 #endif
 
 #if OUR_BDD
-    bdd x2 = bdd_ithvar(2);
     bdd x0 = bdd_ithvar(0);
     bdd x1 = bdd_ithvar(1);
-    bdd intnode = bdd_and(x2, x0);
+    bdd x2 = bdd_ithvar(2);
+    bdd intnode = bdd_and(x0, x2);
     bdd root = bdd_or(intnode, x1);
-    
-    std::vector<label_t> permutation = {1,2,0};
+
+    std::vector<label_t> permutation = {0, 2, 1};
     bdd new_order = bdd_reorder(root, permutation);
-    std::vector<label_t> permutation_inverse = {2,0,1};
+    std::vector<label_t> permutation_inverse = {0, 2, 1};
     bdd org_back = bdd_reorder(new_order, permutation_inverse);
 #endif
 
