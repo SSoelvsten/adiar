41adec2bde7071d161289f82c942ec72e0e5a6ae
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-05-24 14:30:52 +0200

    Inline TPIE wrapper functions

--- src/adiar/internal/memory.h
+++ src/adiar/internal/memory.h
@@ -5,6 +5,9 @@
 
 #include <tpie/tpie.h>
 #include <tpie/memory.h>
+#include <tpie/tempname.h>
+
+#include <adiar/internal/assert.h>
 
 namespace adiar
 {
@@ -15,28 +18,57 @@ namespace adiar
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Sets the path for temporary TPIE files.
     //////////////////////////////////////////////////////////////////////////////
-    void set_path(const std::string &temp_dir = "");
+    inline void set_path(const std::string &temp_dir = "")
+    {
+      // Naming scheme and path for temporary files
+      tpie::tempname::set_default_base_name("ADIAR");
+      tpie::tempname::set_default_extension("adiar");
+
+      if (temp_dir != "") {
+        tpie::tempname::set_default_path(temp_dir);
+      }
+    }
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Sets the memory limit for TPIE.
     //////////////////////////////////////////////////////////////////////////////
-    void set_limit(size_t memory_limit_bytes = MINIMUM_BYTES);
+    inline void set_limit(size_t memory_limit_bytes = MINIMUM_BYTES)
+    {
+      adiar_assert(memory_limit_bytes >= MINIMUM_BYTES,
+                   "Adiar requires at least 128 MiB of memory");
+
+      tpie::get_memory_manager().set_limit(memory_limit_bytes);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// \brief Minimum block size of 2 MiB.
+    //////////////////////////////////////////////////////////////////////////////
+    constexpr size_t MINIMUM_BLOCK_SIZE = 2 * 1024 * 1024;
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Sets the block size for TPIE.
     //////////////////////////////////////////////////////////////////////////////
-    void set_block_size(size_t block_size_bytes);
+    inline void set_block_size(size_t block_size_bytes)
+    {
+      tpie::set_block_size(block_size_bytes);
+    }
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Get the block size from TPIE.
     //////////////////////////////////////////////////////////////////////////////
-    size_t get_block_size();
+    inline size_t get_block_size()
+    {
+      return tpie::get_block_size();
+    }
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Computes a recommended block size to be used with a specific
     ///        amount of available internal memory.
     //////////////////////////////////////////////////////////////////////////////
-    size_t recommended_block_size(size_t memory_limit_bytes);
+    inline size_t recommended_block_size(size_t /*memory_limit_bytes*/)
+    {
+      return MINIMUM_BLOCK_SIZE;
+    }
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Obtain from TPIE the amount of available memory.
