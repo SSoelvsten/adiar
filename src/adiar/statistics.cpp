579b852c164e196afc95a163583c4b38e7cdc23a
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-07-13 15:39:36 +0200

    Slight reformat statistics on priority queue variants

--- src/adiar/statistics.cpp
+++ src/adiar/statistics.cpp
@@ -133,14 +133,16 @@ namespace adiar
     const uintwide_t total_internal_lpqs = stats.lpq.unbucketed + stats.lpq.internal;
 
     indent_level++;
-    o << indent << label << "internal memory"
-      << total_internal_lpqs << " = " << percent_frac(total_internal_lpqs, total_lpqs) << percent << endl;
-
-    o << indent << label << "unbucketed / internal memory"
-      << stats.lpq.unbucketed << " = " << percent_frac(stats.lpq.unbucketed, total_internal_lpqs) << percent << endl;
 
     o << indent << label << "external memory"
       << stats.lpq.external << " = " << percent_frac(stats.lpq.external, total_lpqs) << percent << endl;
+
+    o << indent << label << "internal memory"
+      << total_internal_lpqs << " = " << percent_frac(total_internal_lpqs, total_lpqs) << percent << endl;
+
+    o << indent << label << "  unbucketed"
+      << stats.lpq.unbucketed << " = " << percent_frac(stats.lpq.unbucketed, total_internal_lpqs) << percent << endl;
+
     indent_level--;
 
     o << indent << endl;
