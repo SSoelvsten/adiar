9bd284917c90bf803b1d40d7438175a669447eb4
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-08-01 16:22:40 +0200

    Add Kernel module

--- src/adiar/internal/memory.h
+++ src/adiar/internal/memory.h
@@ -14,16 +14,62 @@ namespace adiar
   namespace memory
   {
     //////////////////////////////////////////////////////////////////////////////
-    /// \brief Choices for whether to use only internal or external memory, or
-    ///        let Adiar choose based on size of input.
+    /// \addtogroup module__adiar
+    ///
+    /// \{
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// \name Memory Mode
+    ///
+    /// Adiar’s Algorithms delay recursion through use of auxiliary data
+    /// structures. These auxiliary data structures can be optimised for
+    /// internal memory, and so have a high performance on very small instances,
+    /// or they can be designed for external memory such that they can handle
+    /// decision diagrams much larger than the available memory.
+    ///
+    /// \{
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// \brief Whether Adiar should exclusively use internal or external memory or
+    ///        automatically pick either type based on size of input.
+    ///
+    /// \details
+    ///   | Enum Value | Effect                                         |
+    ///   |------------|------------------------------------------------|
+    ///   | `AUTO`     | Automatically decide on type of memory to use. |
+    ///   | `INTERNAL` | Always use *internal* memory.                  |
+    ///   | `EXTERNAL` | Always use *external* memory.                  |
     //////////////////////////////////////////////////////////////////////////////
     enum memory_mode { AUTO, INTERNAL, EXTERNAL };
 
     //////////////////////////////////////////////////////////////////////////////
-    /// \brief The current memory mode (set to AUTO as default).
+    /// \brief The current memory mode (default: `AUTO`).
+    ///
+    /// \details
+    ///   If you want to force *Adiar* to a specific memory mode then you should set the
+    ///   global variable `adiar::memory::mode` to one of the above three values. For
+    ///   example, one can force *Adiar* always use internal memory with the
+    ///   following piece of code.
+    ///   ```cpp
+    ///   adiar::memory::mode = adiar::memory::INTERNAL
+    ///   ```
+    ///
+    /// \warning Using `INTERNAL` may lead to crashes if an input or output is
+    ///          too large!
     //////////////////////////////////////////////////////////////////////////////
     extern memory_mode mode;
 
+    /// \}
+    //////////////////////////////////////////////////////////////////////////////
+
+    /// \}
+    //////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// \brief Minimum value of 128 MiB for the memory limit.
+    ///
+    /// \ingroup module__adiar
+    //////////////////////////////////////////////////////////////////////////////
     constexpr size_t MINIMUM_BYTES = 128 * 1024 * 1024;
 
     //////////////////////////////////////////////////////////////////////////////
@@ -42,11 +88,15 @@ namespace adiar
 
     //////////////////////////////////////////////////////////////////////////////
     /// \brief Sets the memory limit for TPIE.
+    ///
+    /// \throws std::invalid_argument if the amount of memory given is smaller
+    ///         than \ref MINIMUM_BYTES
     //////////////////////////////////////////////////////////////////////////////
     inline void set_limit(size_t memory_limit_bytes = MINIMUM_BYTES)
     {
-      adiar_assert(memory_limit_bytes >= MINIMUM_BYTES,
-                   "Adiar requires at least 128 MiB of memory");
+      if (memory_limit_bytes < MINIMUM_BYTES) {
+        throw std::invalid_argument("Adiar requires at least 128 MiB of memory");
+      }
 
       tpie::get_memory_manager().set_limit(memory_limit_bytes);
     }
