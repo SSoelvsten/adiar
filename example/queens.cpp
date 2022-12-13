422a64db09f69c1de2605037f4bbc959de97792b
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-12-13 17:49:15 +0100

    Fix Queens example does not follow v2.0 interface

--- example/queens.cpp
+++ example/queens.cpp
@@ -248,12 +248,12 @@ uint64_t n_queens_list(uint64_t N, uint64_t column,
     partial_assignment.push_back(row_q);
 
     // Construct the assignment for this entire column
-    adiar::shared_file<assignment_t> column_assignment;
+    adiar::shared_file<adiar::assignment> column_assignment;
 
     { // The assignment_writer has to be detached, before we call any bdd
       // functions. It is automatically detached upon destruction, hence we have
       // it in this little scope.
-      adiar::assignment_writer aw(column_assignment);
+      adiar::file_writer<adiar::assignment> aw(column_assignment);
 
       for (uint64_t row = 0; row < N; row++) {
         aw << adiar::create_assignment(label_of_position(N, row, column), row == row_q);
@@ -267,7 +267,7 @@ uint64_t n_queens_list(uint64_t N, uint64_t column,
 
       // Obtain the lexicographically minimal true assignment. Well, only one
       // exists, so we get the only one left.
-      adiar::shared_file<assignment_t> forced_assignment = adiar::bdd_satmin(restricted_constraints);
+      adiar::shared_file<adiar::assignment> forced_assignment = adiar::bdd_satmin(restricted_constraints);
 
       // Sort the variables back in order of the columns, rather than rows.
       struct sort_by_column
@@ -287,7 +287,7 @@ uint64_t n_queens_list(uint64_t N, uint64_t column,
       forced_assignment->sort(sort_by_column(N));
 
       // Copy the rest of the assignment into 'partial_assignment'
-      adiar::internal::file_stream<adiar::assignment> fas(forced_assignment);
+      adiar::file_stream<adiar::assignment> fas(forced_assignment);
       while (fas.can_pull()) {
         adiar::assignment a = fas.pull();
         if (value_of(a)) {
