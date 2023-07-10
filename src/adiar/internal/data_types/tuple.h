7b0a207434fe6085ad3346243d17dbf32cbeba85
Steffan Sølvsten <soelvsten@cs.au.dk>
2023-07-10 19:58:44 +0200

    Switch from pass/return-by-value to pass/return-by-reference in tuple

--- src/adiar/internal/data_types/tuple.h
+++ src/adiar/internal/data_types/tuple.h
@@ -18,57 +18,57 @@ namespace adiar::internal
   //////////////////////////////////////////////////////////////////////////////
   // Ordered access to two elements.
   template<typename elem_t>
-  inline elem_t first(const elem_t t1, const elem_t t2)
+  inline const elem_t& first(const elem_t &t1, const elem_t &t2)
   { return std::min(t1, t2); }
 
   template<typename elem_t>
-  inline elem_t second(const elem_t t1, const elem_t t2)
+  inline const elem_t& second(const elem_t &t1, const elem_t &t2)
   { return std::max(t1, t2); }
 
   //////////////////////////////////////////////////////////////////////////////
   // Ordered access to three elements.
   template<typename elem_t>
-  inline elem_t first(const elem_t t1, const elem_t t2, const elem_t t3)
-  { return std::min({t1, t2, t3}); }
+  inline const elem_t& first(const elem_t &t1, const elem_t &t2, const elem_t &t3)
+  { return std::min(t1, std::min(t2, t3)); }
 
   template<typename elem_t>
-  inline elem_t second(const elem_t t1, const elem_t t2, const elem_t t3)
+  inline const elem_t& second(const elem_t &t1, const elem_t &t2, const elem_t &t3)
   { return std::max(std::min(t1, t2), std::min(std::max(t1,t2),t3)); }
 
   template<typename elem_t>
-  inline elem_t third(const elem_t t1, const elem_t t2, const elem_t t3)
-  { return std::max({t1, t2, t3}); }
+  inline const elem_t& third(const elem_t &t1, const elem_t &t2, const elem_t &t3)
+  { return std::max(t1, std::max(t2, t3)); }
 
   //////////////////////////////////////////////////////////////////////////////
   // Ordered access to four elements.
   template<typename elem_t>
-  inline elem_t first(const elem_t t1, const elem_t t2, const elem_t t3, const elem_t t4)
-  { return std::min({t1, t2, t3, t4}); }
+  inline const elem_t& first(const elem_t &t1, const elem_t &t2, const elem_t &t3, const elem_t &t4)
+  { return std::min(std::min(t1, t2), std::min(t3, t4)); }
 
   template<typename elem_t>
-  inline elem_t second(const elem_t t1, const elem_t t2, const elem_t t3, const elem_t t4)
+  inline const elem_t& second(const elem_t &t1, const elem_t &t2, const elem_t &t3, const elem_t &t4)
   {
     return std::min(std::min(std::max(t1,t2), std::max(t3,t4)),
                     std::max(std::min(t1,t2), std::min(t3,t4)));
   }
 
   template<typename elem_t>
-  inline elem_t third(const elem_t t1, const elem_t t2, const elem_t t3, const elem_t t4)
+  inline const elem_t& third(const elem_t &t1, const elem_t &t2, const elem_t &t3, const elem_t &t4)
   {
     return std::max(std::min(std::max(t1,t2), std::max(t3,t4)),
                     std::max(std::min(t1,t2), std::min(t3,t4)));
   }
 
   template<typename elem_t>
-  inline elem_t fourth(const elem_t t1, const elem_t t2, const elem_t t3, const elem_t t4)
-  { return std::max({t1, t2, t3, t4}); }
+  inline const elem_t& fourth(const elem_t &t1, const elem_t &t2, const elem_t &t3, const elem_t &t4)
+  { return std::max(std::max(t1, t2), std::max(t3, t4)); }
 
   //////////////////////////////////////////////////////////////////////////////
   // TODO (Nested Sweeping Clean Up):
   //   - Map function to create a tuple<B> from tupla<A> and f : A -> B
 
   //////////////////////////////////////////////////////////////////////////////
-  /// \brief Tuple holding two elements and providing an ordered access.
+  /// \brief Tuple holding elements and providing an ordered access.
   ///
   /// \tparam elem_type   The type of elements within the tuple. This part
   ///                     should have a partial ordering '<'.
@@ -87,7 +87,7 @@ namespace adiar::internal
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Type of the elements in the tuple.
     ////////////////////////////////////////////////////////////////////////////
-    typedef elem_type elem_t;
+    using elem_t = elem_type;
 
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Number of elements stored in the tuple.
@@ -104,17 +104,25 @@ namespace adiar::internal
 
     /* ============================== ELEMENTS ============================== */
   private:
-    elem_t _elems [cardinality];
+    std::array<elem_t, cardinality> _elems;
 
     /* ============================ DIRECT ACCESS =========================== */
   public:
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Direct access to tuple elements in the order they were given.
     ////////////////////////////////////////////////////////////////////////////
-    inline elem_t operator [] (const size_t idx) const
+    inline const elem_t& at(const size_t idx) const
     {
       adiar_debug(idx < cardinality, "Tuple index must be within its cardinality.");
       return _elems[idx];
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// \brief Direct access to tuple elements in the order they were given.
+    ////////////////////////////////////////////////////////////////////////////
+    inline const elem_t& operator [] (const size_t idx) const
+    {
+      return this->at(idx);
     }
 
     /* =========================== ORDERED ACCESS =========================== */
@@ -122,13 +130,13 @@ namespace adiar::internal
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Access to the first element wrt. the elements ordering.
     ////////////////////////////////////////////////////////////////////////////
-    inline elem_t first() const
+    inline const elem_t& first() const
     {
       static_assert(1 <= cardinality,
                     "Must at least be a 1-ary tuple to retrieve the first element.");
 
       if constexpr (is_sorted) {
-        return _elems[0];
+        return this->at(0);
       } else if constexpr (cardinality == 2) {
         return adiar::internal::first(_elems[0], _elems[1]);
       } else if constexpr (cardinality == 3) {
@@ -141,13 +149,13 @@ namespace adiar::internal
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Access to the second element wrt. the elements ordering.
     ////////////////////////////////////////////////////////////////////////////
-    inline elem_t second() const
+    inline const elem_t& second() const
     {
       static_assert(2 <= cardinality,
                     "Must at least be a 2-ary tuple to retrieve the second element.");
 
       if constexpr (is_sorted) {
-        return _elems[1];
+        return this->at(1);
       } else if constexpr (cardinality == 2) {
         return adiar::internal::second(_elems[0], _elems[1]);
       } else if constexpr (cardinality == 3) {
@@ -160,13 +168,13 @@ namespace adiar::internal
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Access to the second element wrt. the elements ordering.
     ////////////////////////////////////////////////////////////////////////////
-    inline elem_t third() const
+    inline const elem_t& third() const
     {
       static_assert(3 <= cardinality,
                     "Must at least be a 3-ary tuple to retrieve the third element.");
 
       if constexpr (is_sorted) {
-        return _elems[2];
+        return this->at(2);
       } else if constexpr (cardinality == 3) {
         return adiar::internal::third(_elems[0], _elems[1], _elems[2]);
       } else if constexpr (cardinality == 4) {
@@ -177,13 +185,13 @@ namespace adiar::internal
     ////////////////////////////////////////////////////////////////////////////
     /// \brief Access to the second element wrt. the elements ordering.
     ////////////////////////////////////////////////////////////////////////////
-    inline elem_t fourth() const
+    inline const elem_t& fourth() const
     {
       static_assert(3 <= cardinality,
                     "Must at least be a 4-ary tuple to retrieve the fourth element.");
 
       if constexpr (is_sorted) {
-        return _elems[3];
+        return this->at(3);
       } else if constexpr (cardinality == 4) {
         return adiar::internal::fourth(_elems[0], _elems[1], _elems[2], _elems[3]);
       }
@@ -241,7 +249,7 @@ namespace adiar::internal
     }
 
     ////////////////////////////////////////////////////////////////////////////
-    /// \brief Create a 4-ary tuple with the three given elements.
+    /// \brief Create a 4-ary tuple with the four given elements.
     ////////////////////////////////////////////////////////////////////////////
     tuple(const elem_t &elem1,
           const elem_t &elem2,
