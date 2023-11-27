3be98c53ca21f28f4da09494f33fc9936c263c67
Steffan Sølvsten <soelvsten@cs.au.dk>
2023-11-27 14:52:43 -0500

    Cleaner code for 'internal/algorithms/traverse.h'

--- src/adiar/internal/algorithms/traverse.h
+++ src/adiar/internal/algorithms/traverse.h
@@ -10,13 +10,22 @@
 
 namespace adiar::internal
 {
-  template<typename dd_t, typename traverse_visitor>
-  void traverse(const dd_t &dd, traverse_visitor &visitor)
+  //////////////////////////////////////////////////////////////////////////////
+  //  Traverse Algorithm
+  // ====================
+  //
+  // Traversal of a Decision Diagram together with callbacks for a Visitor to
+  // follow along and pick whereto go.
+  //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  template<typename Dd, typename Visitor>
+  void traverse(const Dd &dd, Visitor &visitor)
   {
     node_stream<> in_nodes(dd);
 
-    typename dd_t::node_type n   = in_nodes.pull();
-    typename dd_t::pointer_type  tgt = n.uid();
+    typename Dd::node_type n      = in_nodes.pull();
+    typename Dd::pointer_type tgt = n.uid();
 
     while (!tgt.is_terminal() && !tgt.is_nil()) {
       while (n.uid() < tgt) { n = in_nodes.pull(); }
@@ -27,40 +36,50 @@ namespace adiar::internal
       tgt = visitor.visit(n);
 
       adiar_assert((tgt == n.low()) || (tgt == n.high()) || (tgt.is_nil()),
-                   "Visitor pointer should be within the diagram or nil");
+                   "Visitor pointer should be a child or nil");
     }
     if (!tgt.is_nil()) {
       visitor.visit(tgt.value());
     }
   }
 
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief Implementation of Visitor logic, traversing the lexicographically
+  ///        smallest assignment.
+  ///
+  /// \details Picks the high arc as long as it does not goto `false`.
+  //////////////////////////////////////////////////////////////////////////////
   class traverse_satmin_visitor
   {
   public:
     static constexpr bool default_direction = false;
 
-    inline ptr_uint64 visit(const node &n)
+    inline node::pointer_type visit(const node &n)
     {
-      // Only pick high, if low is the false terminal
       return n.low().is_false() ? n.high() : n.low();
     }
 
-    inline void visit(const bool /*s*/)
+    inline void visit(const bool/*t*/)
     { }
   };
 
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief Implementation of Visitor logic, traversing the lexicographically
+  ///        largest assignment.
+  ///
+  /// \details Picks the low arc as long as it does not goto the `false`.
+  //////////////////////////////////////////////////////////////////////////////
   class traverse_satmax_visitor
   {
   public:
     static constexpr bool default_direction = true;
 
-    inline ptr_uint64 visit(const node &n)
+    inline node::pointer_type visit(const node &n)
     {
-      // Pick high as long it is not the false terminal
-      return n.high().is_node() || n.high().value() ? n.high() : n.low();
+      return n.high().is_false() ? n.low() : n.high();
     }
 
-    inline void visit(const bool /*s*/)
+    inline void visit(const bool/*t*/)
     { }
   };
 }
