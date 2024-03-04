6025ee7e12610df660f7c6262b219ead16f361ec
Steffan Sølvsten <soelvsten@cs.au.dk>
2024-03-04 13:21:06 +0100

    Split test file for boolean operators

--- test/adiar/test_bool_op.cpp
+++ test/adiar/test_bool_op.cpp
@@ -1,9 +1,7 @@
 #include "../test.h"
 
-#include <adiar/internal/bool_op.h>
-
 go_bandit([]() {
-  describe("adiar/bool_op.h / adiar/internal/bool_op.h", []() {
+  describe("adiar/bool_op.h", []() {
     it("and_op", [&]() {
       AssertThat(and_op(true, true), Is().EqualTo(true));
       AssertThat(and_op(true, false), Is().EqualTo(false));
@@ -79,129 +77,6 @@ go_bandit([]() {
       AssertThat(less_op(true, false), Is().EqualTo(false));
       AssertThat(less_op(false, true), Is().EqualTo(true));
       AssertThat(less_op(false, false), Is().EqualTo(false));
-    });
-
-    describe("can_shortcut", [&]() {
-      it("can check on 'true' on the left", [&]() {
-        AssertThat(can_left_shortcut(and_op, true), Is().False());
-        AssertThat(can_left_shortcut(or_op, true), Is().True());
-        AssertThat(can_left_shortcut(xor_op, true), Is().False());
-        AssertThat(can_left_shortcut(imp_op, true), Is().False());
-      });
-
-      it("can check on 'false' on the left", [&]() {
-        AssertThat(can_left_shortcut(and_op, false), Is().True());
-        AssertThat(can_left_shortcut(or_op, false), Is().False());
-        AssertThat(can_left_shortcut(xor_op, false), Is().False());
-        AssertThat(can_left_shortcut(imp_op, false), Is().True());
-      });
-
-      it("can check on 'true' on the right", [&]() {
-        AssertThat(can_right_shortcut(and_op, true), Is().False());
-        AssertThat(can_right_shortcut(or_op, true), Is().True());
-        AssertThat(can_right_shortcut(xor_op, true), Is().False());
-        AssertThat(can_right_shortcut(imp_op, true), Is().True());
-      });
-
-      it("can check on 'false' on the right", [&]() {
-        AssertThat(can_right_shortcut(and_op, false), Is().True());
-        AssertThat(can_right_shortcut(or_op, false), Is().False());
-        AssertThat(can_right_shortcut(xor_op, false), Is().False());
-        AssertThat(can_right_shortcut(imp_op, false), Is().False());
-      });
-    });
-
-    describe("is_idempotent", [&]() {
-      it("can check on 'true' on the left", [&]() {
-        AssertThat(is_left_idempotent(and_op, true), Is().True());
-        AssertThat(is_left_idempotent(or_op, true), Is().False());
-        AssertThat(is_left_idempotent(xor_op, true), Is().False());
-        AssertThat(is_left_idempotent(imp_op, true), Is().True());
-      });
-
-      it("can check on 'false' on the left", [&]() {
-        AssertThat(is_left_idempotent(and_op, false), Is().False());
-        AssertThat(is_left_idempotent(or_op, false), Is().True());
-        AssertThat(is_left_idempotent(xor_op, false), Is().True());
-        AssertThat(is_left_idempotent(imp_op, false), Is().False());
-      });
-
-      it("can check on 'true' on the right", [&]() {
-        AssertThat(is_right_idempotent(and_op, true), Is().True());
-        AssertThat(is_right_idempotent(or_op, true), Is().False());
-        AssertThat(is_right_idempotent(xor_op, true), Is().False());
-        AssertThat(is_right_idempotent(imp_op, true), Is().False());
-      });
-
-      it("can check on 'false' on the right", [&]() {
-        AssertThat(is_right_idempotent(and_op, false), Is().False());
-        AssertThat(is_right_idempotent(or_op, false), Is().True());
-        AssertThat(is_right_idempotent(xor_op, false), Is().True());
-        AssertThat(is_right_idempotent(imp_op, false), Is().False());
-      });
-    });
-
-    describe("is_negating", [&]() {
-      it("can check on 'true' on the left", [&]() {
-        AssertThat(is_left_negating(and_op, true), Is().False());
-        AssertThat(is_left_negating(or_op, true), Is().False());
-        AssertThat(is_left_negating(xor_op, true), Is().True());
-        AssertThat(is_left_negating(imp_op, true), Is().False());
-      });
-
-      it("can check on 'false' on the left", [&]() {
-        AssertThat(is_left_negating(and_op, false), Is().False());
-        AssertThat(is_left_negating(or_op, false), Is().False());
-        AssertThat(is_left_negating(xor_op, false), Is().False());
-        AssertThat(is_left_negating(imp_op, false), Is().False());
-      });
-
-      it("can check on 'true' on the right", [&]() {
-        AssertThat(is_right_negating(and_op, true), Is().False());
-        AssertThat(is_right_negating(or_op, true), Is().False());
-        AssertThat(is_right_negating(xor_op, true), Is().True());
-        AssertThat(is_right_negating(imp_op, true), Is().False());
-      });
-
-      it("can check on 'false' on the right", [&]() {
-        AssertThat(is_right_negating(and_op, false), Is().False());
-        AssertThat(is_right_negating(or_op, false), Is().False());
-        AssertThat(is_right_negating(xor_op, false), Is().False());
-        AssertThat(is_right_negating(imp_op, false), Is().False());
-      });
-    });
-
-    it("can check the operators for being commutative", []() {
-      AssertThat(is_commutative(and_op), Is().True());
-      AssertThat(is_commutative(nand_op), Is().True());
-      AssertThat(is_commutative(or_op), Is().True());
-      AssertThat(is_commutative(nor_op), Is().True());
-      AssertThat(is_commutative(xor_op), Is().True());
-      AssertThat(is_commutative(imp_op), Is().False());
-      AssertThat(is_commutative(invimp_op), Is().False());
-      AssertThat(is_commutative(equiv_op), Is().True());
-      AssertThat(is_commutative(diff_op), Is().False());
-      AssertThat(is_commutative(diff_op), Is().False());
-    });
-
-    describe("flip operator", [&]() {
-      it("resolves commutative operator (and)", [&]() {
-        const bool_op& flip_and_op = flip(and_op);
-
-        AssertThat(flip_and_op(true, true), Is().EqualTo(true));
-        AssertThat(flip_and_op(true, false), Is().EqualTo(false));
-        AssertThat(flip_and_op(false, true), Is().EqualTo(false));
-        AssertThat(flip_and_op(false, false), Is().EqualTo(false));
-      });
-
-      it("resolves non-commutative operator (imp)", [&]() {
-        const bool_op& flip_imp_op = flip(imp_op);
-
-        AssertThat(flip_imp_op(true, true), Is().EqualTo(true));
-        AssertThat(flip_imp_op(true, false), Is().EqualTo(true));
-        AssertThat(flip_imp_op(false, true), Is().EqualTo(false));
-        AssertThat(flip_imp_op(false, false), Is().EqualTo(true));
-      });
     });
   });
 });
