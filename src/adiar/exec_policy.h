c487a97b83f428a14bd9bcce4dc8f334c0d328f9
Steffan Sølvsten <soelvsten@cs.au.dk>
2024-03-11 17:32:43 +0100

    Set 'transposition growth' to unbounded increase

--- src/adiar/exec_policy.h
+++ src/adiar/exec_policy.h
@@ -150,7 +150,7 @@ namespace adiar
 
       public:
         constexpr transposition_growth()
-          : _value(1.5f)
+          : _value(std::numeric_limits<float>::max())
         {}
 
         /// \brief Wrap a `float`.
